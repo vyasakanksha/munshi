dc374bcdf637484235c1b34f36b84f2a0dc30ca2
Akanksha Vyas <vyasakanksha@gmail.com>
2019-08-06 19:55:02 +0530

    Added upload audio endpoint

--- munshi_app.py
+++ munshi_app.py
@@ -1,10 +1,11 @@
 import logging, json
-from flask import current_app, Flask, render_template, request
+from flask import current_app, Flask, render_template, request, jsonify, make_response
 import crud, transcribe_async as transcribe
 
 app = Flask(__name__)
 
 bucket = "barricade_transcript"
+project_name = "bar"
 
 # Configure logging
 if not app.testing:
@@ -16,6 +17,35 @@ def home():
    files = crud.blobs_list(bucket)
    audio_files = [x for x,y in files if y == '.wav']
    return render_template('home.html', audio_files=audio_files)
+
+@app.route("/record", methods=['GET', 'POST'])
+def record_audio():
+   return render_template('record_audio.html')
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_audio():
+	files = crud.blobs_list(bucket)
+	audio_files = [x for x,y in files if y == '.wav']
+
+   # generate the name for the next audio file in series by incementing the number
+	if audio_files:
+		res1 = list(map(lambda sub:int(''.join([ele for ele in sub if ele.isnumeric()])), audio_files))
+		res2 = list(map(lambda sub:str(''.join([ele for ele in sub if not ele.isnumeric()])), audio_files))
+		filename = "{}{}".format(res2[0], str(max(res1)+1))
+	else:
+		filename = "{}{}".format(project_name, 1)
+
+	if request.method == 'POST':
+		stat = request.files['data']
+		stat.save("copy.wav")
+		metadata = {'status': 0} # set status to new
+		try:
+			upload = crud.upload_blob(bucket, "copy.wav", stat.filename, metadata)
+			return json.dumps({'status':'OK'})
+		except Exception as e:
+			return jsonify({'error' : e})
+	return render_template('upload_audio.html', filename=filename)
+
 
 @app.route("/edit/<filename>", methods=['GET', 'POST'])
 def edit(filename):
