84d5da3f40611458d1eb53f78ae8601a4d156787
Akanksha Vyas <vyasakanksha@gmail.com>
2021-06-23 00:06:02 +0530

    playing with the stream and button toggle

--- munshi-app/src/App.vue
+++ munshi-app/src/App.vue
@@ -8,18 +8,17 @@
         >
         <!-- <v-icon v-if="isRecording"><i class='fa fa-microphone fa-2x'></i></v-icon>
         <v-icon v-else><i class='fa fa-microphone-slash fa-2x'></i></v-icon> -->
-        <v-icon v-if="isRecording">fas fa-microphone fa-2x</v-icon>
-        <v-icon v-else>mdi-volume-high</v-icon>
+        <v-icon v-if="isRecording"><font-awesome-icon icon="microphone" /></v-icon>
+        <v-icon v-else><font-awesome-icon icon="microphone-slash" /></v-icon>
         </v-btn>
       </div>
-      <Recorder ref="myRecorder" />
     <Editor/>
   </div>
 </template>
 
 <script>
 import Editor from './components/Editor.vue'
-import Recorder from './components/Recorder.vue'
+import { RecordRTCPromisesHandler } from "recordrtc";
 
 
 export default {
@@ -34,38 +33,24 @@ export default {
   },
   components: {
     Editor,
-    Recorder
   },
   methods: {
-    AudioRecorded() {
-        this.isRecording = !this.isRecording;
-        console.log("isRecording", this.isRecording)
+    async AudioRecorded() {
         if(!this.isRecording) {
           console.log("Start Recoding")
-        //   var device = navigator.mediaDevices.getUserMedia({ audio: true });
-        //   device.then((stream) => {
-        //     // console.log(stream)
-        //     // use this!
-        //     this.recorder = new MediaRecorder(stream);
-        //     this.recorder.ondataavailable = (e) => {
-        //         console.log(e)
-        //         if (this.recorder.state == "inactive") {
-        //           var blob = new Blob(this.items, { type: "audio/*" });
-        //           var audio = document.getElementById("audio");
-        //           var mainaudio = document.createElement("audio");
-        //           mainaudio.setAttribute("controls", "controls");
-        //           audio.appendChild(mainaudio);
-        //           mainaudio.innerHTML =
-        //               '<source src="' +
-        //               URL.createObjectURL(blob) +
-        //               '" type="audio/*" />';
-        //       }
-        //     };
-        //     this.recorder.start();
-        //   });
+          let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+          var recorder = new RecordRTCPromisesHandler(stream, {
+              type: 'video'
+          });
+          recorder.startRecording();
+          this.isRecording = false;
+          
         } else if(this.isRecording) {
-          console.log("Stop Recoding")
-        //   this.recorder.stop();
+          this.isRecording = false;
+          console.log("isRecording", this.isRecording)
+
+          await recorder.stopRecording();
+          // let blob = await recorder.getBlob();
         }
     },
   }
